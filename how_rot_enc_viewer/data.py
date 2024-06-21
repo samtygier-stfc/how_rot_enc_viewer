b533e0f543d77690c12b63097ba4713856e8cdb2
Sam Tygier <sam.tygier@stfc.ac.uk>
2024-06-21 08:47:59 +0100

    Add data loading

---
+++ how_rot_enc_viewer/data.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+from collections.abc import Sequence
+
+import numpy as np
+from tifffile import tifffile
+
+
+def imread(filename: Path) -> np.ndarray:
+    try:
+        return tifffile.imread(filename)
+    except tifffile.TiffFileError as e:
+        raise RuntimeError(f"TiffFileError {e.args[0]}: {filename}") from e
+
+
+def load_stack(directory: Path, name_pattern: str,
+               indexes: Sequence[int]) -> np.ndarray:
+    image_0 = imread(directory / name_pattern.format(indexes[0]))
+
+    count = len(indexes)
+    width, height = image_0.shape
+    image_stack = np.zeros((count, height, width), dtype=image_0.dtype)
+
+    for i in indexes:
+        image_stack[i] = imread(directory / name_pattern.format(i)).T[:, ::-1]
+
+    return image_stack
